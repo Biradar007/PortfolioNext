8e1727a2af703cb72bcfe77efc40ed99c6a2b094
ubiradar <udaybiradar007@gmail.com>
2024-12-27 13:07:00 -0800

    minor changes

--- uday-portfolio/src/app/components/Skills.tsx
+++ uday-portfolio/src/app/components/Skills.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Link } from 'react-scroll';
+// import { Link } from 'react-scroll';
 import { motion } from "framer-motion";
 import {
   FaHtml5,
